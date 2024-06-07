116d647b37c75d73138eedcf0b77c6d4cdedc62d
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-06-07 16:48:11 +0200

    Solution by Isaias Xavier

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\Stat;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
 {
+    use SoftDeletes;
     public function store(Request $request)
     {
         // TASK: Currently this statement fails. Fix the underlying issue.
+        $validated = $request->validate([
+            'name' => 'required|max:255'
+        ]);
+
         Project::create([
-            'name' => $request->name
+            'name' => $validated['name']
         ]);
 
         return redirect('/')->with('success', 'Project created');
@@ -25,7 +32,15 @@ class ProjectController extends Controller
         //   set name = $request->new_name
         //   where name = $request->old_name
 
+        //update projects set name = $request->new_name where name = $request->old_name
         // Insert Eloquent statement below
+        $old_name = $request->old_name;
+        $new_name = $request->new_name;
+
+        $update = DB::table('projects')
+            ->where('name', $old_name)
+            ->update(['name' => $new_name]);
+
 
         return redirect('/')->with('success', 'Projects updated');
     }
@@ -35,7 +50,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive()
+    {
+        return self::whereNotNull('email_verified_at');
+    }
+
+
 }
